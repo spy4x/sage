18247e733eda8f29bf07c0096541ce74f95af090
Anton Shubin <2spy4x@gmail.com>
2023-07-14 12:02:02 +0800

    Add domain link

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,7 @@
 import type { SSTConfig } from 'sst';
 import { SvelteKitSite } from 'sst/constructs';
 import { config } from 'dotenv';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 const { parsed: environment } = config();
 
 export default {
@@ -15,9 +16,23 @@ export default {
       const site = new SvelteKitSite(stack, 'site', {
         timeout: '60 seconds',
         environment,
+        customDomain:
+          app.stage === 'prod'
+            ? {
+                domainName: 'sage.antonshubin.com',
+                isExternalDomain: true,
+                cdk: {
+                  certificate: Certificate.fromCertificateArn(
+                    stack,
+                    'MyCert',
+                    'arn:aws:acm:us-east-1:232783690572:certificate/17b62498-f6d6-4b90-a907-48b32c92253a',
+                  ),
+                },
+              }
+            : undefined,
       });
       stack.addOutputs({
-        url: site.url,
+        url: site.customDomainUrl || site.url,
       });
     });
   },
