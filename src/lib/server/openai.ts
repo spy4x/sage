535ed885e9d80ca610c3c793239a09a282c45bbd
Anton Shubin <2spy4x@gmail.com>
2023-09-25 20:17:47 +0800

    env: configure deploy to a server that hosts multiple apps

--- src/lib/server/openai.ts
+++ src/lib/server/openai.ts
@@ -1,8 +1,16 @@
 import { env } from '$env/dynamic/private';
 import OpenAI from 'openai';
 
+const apiKey = env.OPENAI_API_KEY;
+
+if (!apiKey) {
+  console.error(
+    'No OpenAI API key found in environment variables. OpenAI API will not be available.',
+  );
+}
+
 const openai = new OpenAI({
-  apiKey: env.OPENAI_API_KEY,
+  apiKey: apiKey || 'sk-fake',
 });
 
 export { openai };
