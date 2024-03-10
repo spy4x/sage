7f87723da3f7675edaa7c5053f9b9f0354d8ad50
spy4x <2spy4x@gmail.com>
2024-03-10 20:56:28 +0800

    feat: add "Send" button, use Enter+Shift to send via hotkey

--- src/lib/client/components/index.ts
+++ src/lib/client/components/index.ts
@@ -4,3 +4,4 @@ export { default as CopyToClipboard } from './copyToClipboard.svelte';
 export { default as FormError } from './formError.svelte';
 export { default as ChatMessage } from './chat-message.svelte';
 export { default as ModelSelector } from './model-selector.svelte';
+export * from './icons';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import { AsyncOperationStatus, EntityOperationType, MessageSchema, type Message } from '@shared';
   import { chats } from '@stores';
   import { onMount } from 'svelte';
-  import { ChatMessage, Debug, Loading, ModelSelector } from '@components';
+  import { ChatMessage, Debug, IconSend, Loading, ModelSelector } from '@components';
 
   let elemChat: HTMLElement;
   let currentMessage = '';
@@ -31,7 +31,7 @@
   function submitOnEnter(e: KeyboardEvent): void {
     const specialKey = e.metaKey || e.ctrlKey || e.shiftKey || e.altKey;
     // if Special key + Enter = new line
-    if (e.key === 'Enter' && !specialKey) {
+    if (e.key === 'Enter' && specialKey) {
       e.preventDefault();
       addMessage();
     }
@@ -95,21 +95,26 @@
     <!-- #endregion -->
 
     <!-- #region Prompt -->
-    <div class="w-full text-right">
+    <div class="w-full">
       <div class="mb-2">
         <ModelSelector model={$chats.chat.model} on:change={e => chats.setModel(e.detail)} />
       </div>
-      <textarea
-        bind:value={currentMessage}
-        bind:this={messageInput}
-        class="block w-full rounded-md border-0 py-1.5 bg-slate-100 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 min-h-[20px]"
-        name="prompt"
-        id="prompt"
-        placeholder="Write a message..."
-        rows="1"
-        on:keydown={submitOnEnter}
-        on:input={resizeTextarea}
-      />
+      <div class="flex gap-1 items-start">
+        <textarea
+          bind:value={currentMessage}
+          bind:this={messageInput}
+          class="block w-full rounded-md border-0 py-1.5 bg-slate-100 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 min-h-[32px] max-h-[200px]"
+          name="prompt"
+          id="prompt"
+          placeholder="Write a message..."
+          rows="1"
+          on:keydown={submitOnEnter}
+          on:input={resizeTextarea}
+        />
+        <button class="btn variant-primary" on:click={addMessage} title="Hotkey: Enter+Shift">
+          <IconSend />
+        </button>
+      </div>
     </div>
     <!-- #endregion -->
   </div>
