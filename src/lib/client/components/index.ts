4c8ab21f43bae5528c824fa83bc1f0d634b316e5
spy4x <2spy4x@gmail.com>
2024-03-10 20:34:09 +0800

    ui: make "Copy answer" button change to "☑ Copied" for 3 seconds after click

--- src/lib/client/components/index.ts
+++ src/lib/client/components/index.ts
@@ -1,6 +1,6 @@
 export { default as Loading } from './loading.svelte';
 export { default as Debug } from './debug.svelte';
-export { default as CopyLink } from './copyLink.svelte';
+export { default as CopyToClipboard } from './copyToClipboard.svelte';
 export { default as FormError } from './formError.svelte';
 export { default as ChatMessage } from './chat-message.svelte';
 export { default as ModelSelector } from './model-selector.svelte';
