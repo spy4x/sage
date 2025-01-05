650346fe6de17e1376d3dd97e51a5eb546eaf943
Anton Shubin <2spy4x@gmail.com>
2025-01-05 17:04:13 +0700

    fix: more convenient page password protection, update model to latest

--- src/lib/shared/types.ts
+++ src/lib/shared/types.ts
@@ -71,7 +71,7 @@ export enum Role {
 }
 export enum Model {
   GPT3 = 'gpt-3.5-turbo',
-  GPT4 = 'gpt-4o-2024-08-06',
+  GPT4 = 'gpt-4o-2024-11-20',
 }
 export interface Persona {
   id: number;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,44 @@
 <script lang="ts">
   import '../app.css';
+  import { browser } from '$app/environment';
+  import { env } from '$env/dynamic/public';
+  let password = browser ? localStorage.getItem('password') ?? '' : '';
+  let isSignedIn = password === env.PUBLIC_PASSWORD;
+  let isButtonClicked = false;
+
+  function submitPassword() {
+    isButtonClicked = true;
+    if (password === env.PUBLIC_PASSWORD) {
+      localStorage.setItem('password', password);
+      isSignedIn = true;
+    }
+  }
 </script>
 
 <div class="p-4 lg:p-6 hd-full">
-  <slot />
+  <!-- If isSignedIn - slow <slot/>, otherwise - password input -->
+  {#if isSignedIn}
+    <slot />
+  {:else}
+    <form on:submit={submitPassword} class="flex flex-col gap-4 items-center">
+      <!-- Add keydown:enter event -->
+      <input
+        type="password"
+        bind:value={password}
+        placeholder="Password"
+        class="block rounded-md border-0 py-1.5 bg-slate-100 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      />
+      <button class="btn variant-primary"> Sign in </button>
+
+      {#if password}
+        {#if isButtonClicked}
+          <p class="text-sm text-center opacity-50">Incorrect password. Please try again.</p>
+        {/if}
+      {:else}
+        <p class="text-sm text-center opacity-50">
+          This page is password protected. Please enter the password to continue.
+        </p>
+      {/if}
+    </form>
+  {/if}
 </div>
